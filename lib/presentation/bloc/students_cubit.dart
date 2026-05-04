ba48fd7d94e25d5e2272f92f39765643cd2e2191
Меруерт Кожахмет <meruertkozahmet@MacBook-Air-Meruert.local>
2026-05-04 12:49:11 +0400

    writing right imports

--- lib/presentation/bloc/students_cubit.dart
+++ lib/presentation/bloc/students_cubit.dart
@@ -1,5 +1,5 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
-import '../../data/repositories/student_repository.dart';
+import '../../domain/repositories/student_repository.dart';
 
 class StudentCubit extends Cubit<List> {
   final repo;
