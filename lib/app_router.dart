a7955662a8946323ae705908f0d60563c000e5c9
Меруерт Кожахмет <meruertkozahmet@MacBook-Air-Meruert.local>
2026-05-04 12:40:53 +0400

    update main and app_router

--- lib/app_router.dart
+++ lib/app_router.dart
@@ -1,18 +1,12 @@
 import 'package:go_router/go_router.dart';
-
-import 'presentation/screens/login_screen.dart';
 import 'presentation/screens/home_screen.dart';
 import 'presentation/screens/students_screen.dart';
-import 'presentation/screens/search_screen.dart';
 import 'presentation/screens/charts_screen.dart';
 
 final router = GoRouter(
-  initialLocation: '/login',
   routes: [
-    GoRoute(path: '/login', builder: (c, s) => const LoginScreen()),
-    GoRoute(path: '/home', builder: (c, s) => const HomeScreen()),
-    GoRoute(path: '/students', builder: (c, s) => const StudentsScreen()),
-    GoRoute(path: '/search', builder: (c, s) => const SearchScreen()),
-    GoRoute(path: '/charts', builder: (c, s) => const ChartsScreen()),
+    GoRoute(path: '/', builder: (_, __) => const HomeScreen()),
+    GoRoute(path: '/students', builder: (_, __) => const StudentsScreen()),
+    GoRoute(path: '/charts', builder: (_, __) => const ChartsScreen()),
   ],
 );
--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'app_router.dart';
+import 'data/repositories/student_repository.dart';
+import 'presentation/bloc/student_cubit.dart';
+import 'presentation/bloc/tracking_cubit.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Hive.initFlutter();
   await Hive.openBox('studentsBox');
 
-  runApp(const MyApp());
+  final repo = StudentRepository();
+
+  runApp(MyApp(repo));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final StudentRepository repo;
+  const MyApp(this.repo, {super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      debugShowCheckedModeBanner: false,
-      routerConfig: router,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
-        useMaterial3: true,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (_) => StudentCubit(repo)),
+        BlocProvider(create: (_) => TrackingCubit(repo)),
+      ],
+      child: MaterialApp.router(
+        debugShowCheckedModeBanner: false,
+        routerConfig: router,
+        theme: ThemeData(
+          useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+          scaffoldBackgroundColor: const Color(0xFFF5F7FA),
+        ),
       ),
     );
   }
