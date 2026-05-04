016cdce69ba1702ff64233d74bae73996faa5b74
Меруерт Кожахмет <meruertkozahmet@MacBook-Air-Meruert.local>
2026-05-04 12:47:16 +0400

    creating students_cubit.dart

---
+++ lib/presentation/bloc/students_cubit.dart
@@ -0,0 +1,21 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import '../../data/repositories/student_repository.dart';
+
+class StudentCubit extends Cubit<List> {
+  final repo;
+  StudentCubit(this.repo) : super([]) {
+    load();
+  }
+
+  void load() => emit(repo.getAll());
+
+  void add(String name) {
+    repo.add(name);
+    load();
+  }
+
+  void delete(int i) {
+    repo.delete(i);
+    load();
+  }
+}
